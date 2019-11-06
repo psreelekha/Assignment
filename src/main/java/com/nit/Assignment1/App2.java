f44b5b949999bc68a5d70d67e85a98c6c0b8e477
Hemanth kumar <Hemanth kumar@192.168.100.24>
2019-11-06 19:35:24 +0530

    committing App2 to BranchA

---
+++ src/main/java/com/nit/Assignment1/App2.java
@@ -0,0 +1,13 @@
+package com.nit.Assignment1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App2 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello!" );
+    }
+}
