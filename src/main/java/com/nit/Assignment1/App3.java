efa9b9190df87fea6f494e2baa3f62c0dae1df16
Hemanth kumar <Hemanth kumar@192.168.100.24>
2019-11-06 22:04:29 +0530

    App3 committed

---
+++ src/main/java/com/nit/Assignment1/App3.java
@@ -0,0 +1,13 @@
+package com.nit.Assignment1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App3 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello!" );
+    }
+}
